910485190aedd39636fc51a0382cb73f9a0943a4
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-09-12 14:37:56 +0530

    Add files via upload

--- Javascript Challenge/static/js/script.js
+++ Javascript Challenge/static/js/script.js
@@ -15,3 +15,12 @@ function ageInDays(){
 function reset(){
     document.getElementById("ageInDays").remove();
 }
+
+//Challenge 2: Cat Generator
+function generateCat(){
+    var image= document.createElement('img');
+    var div= document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small"
+    div.appendChild(image);
+    console.log("cat generated");
+}
