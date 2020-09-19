516f75728f2c2540441641fdd6357188086784ff
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-09-19 12:12:27 +0530

    Add files via upload

--- Javascript Challenge/static/js/script.js
+++ Javascript Challenge/static/js/script.js
@@ -24,3 +24,65 @@ function generateCat(){
     div.appendChild(image);
     console.log("cat generated");
 }
+
+//Challenge 3: Rock, Paper and Scissors
+function decideWinner(humanChoice,botChoice){
+    var rpsDatabase={
+        'rock':{'rock':0.5 ,'paper':0 , "scissors":1 },
+        'paper':{'rock':1 ,'paper':0.5 , "scissors":0 },
+        'scissors':{'rock':0 ,'paper':1 , "scissors":0.5 }
+    };
+    return rpsDatabase[humanChoice][botChoice];
+}
+
+function finalMessage(yourScore){
+    var rpsMessage={
+        0:{'message':'You Lost!',"color":"red" },
+        0.5:{'message':'You Tied! ',"color":"yellow" },
+        1:{'message':'You Won!',"color":"green" }
+    }
+    return rpsMessage[yourScore]
+}
+
+function rpsFrontEnd(humanChoice,botChoice,fmessage){
+    var humanImage=document.getElementById(humanChoice);
+    var botImage=document.getElementById(botChoice);
+
+    //removing all images
+    document.getElementById("rock").remove();
+    document.getElementById("paper").remove();
+    document.getElementById("scissors").remove();
+
+    var humanDiv=document.createElement("div");
+    var botDiv=document.createElement("div");
+    var messageDiv=document.createElement("div");
+    
+    humanDiv.innerHTML="<img src='"+humanImage.src+"' height=250 width=250 style='box-shadow: 0px 10px 50px rgba( 37, 50 , 233 , 1 );' >";
+    messageDiv.innerHTML="<h2 style='color : "+ fmessage["color"] + "; font-size: 60px; padding: 30px ; '> " + fmessage["message"] + " </h2> ";
+    botDiv.innerHTML="<img src='"+botImage.src+"' height=250 width=250 style='box-shadow: 0px 10px 50px rgba( 243 , 38 , 24 , 1 );' >";
+
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+}
+
+function rpsReset(){
+    var node=document.getElementById('flex-box-rps-div');
+
+    node.innerHTML=" <img id ='rock' src='https://purepng.com/public/uploads/large/dwayne-johnson-the-rock-hbz.png' height=250 width=250 onclick='rpsGame(this)'> \
+    <img id ='paper' src='https://img.favpng.com/12/5/10/towel-kitchen-paper-tissue-paper-toilet-paper-png-favpng-Uz6vWCeWVm4YGpf4VdjX7AADz.jpg' height=250 width=250 onclick='rpsGame(this)'> \
+    <img id ='scissors' src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/still10-preview-1522353855.jpg?crop=1.00xw:0.752xh;0,0.0625xh&resize=640:*' height=250 width=250 onclick='rpsGame(this)' >";
+    console.log("resetting");
+}
+function rpsGame(yourChoice){
+    var humanChoice, botChoice,results;
+    humanChoice=yourChoice.id;
+    botChoice= ["rock","paper","scissors"][(Math.floor(Math.random()*10))%3];
+    results=decideWinner(humanChoice,botChoice);
+    console.log("computer: ", botChoice);
+    message=finalMessage(results);
+    console.log(message);
+    rpsFrontEnd(humanChoice,botChoice,message);
+
+}
+
